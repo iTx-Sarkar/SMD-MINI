d36d8ab49d4e127159ea25ba736166bd694583b7
iTx Sarkar <project01bybandaheali@gmail.com>
2026-03-16 04:48:00 +0500

    Update smd-mini.js

--- data/smd-mini.js
+++ data/smd-mini.js
@@ -1,6 +1,13 @@
 const smd = require("team-bandaheali");
 const db = smd.db;
 
-async function connectDb () = {
-  db.connect();
-  console.log("smd-mini db connected");
+async function connectDb() {
+  try {
+    await db.connect();
+    console.log("smd-mini db connected");
+  } catch (err) {
+    console.error("Database connection failed:", err);
+  }
+}
+
+module.exports = connectDb;
